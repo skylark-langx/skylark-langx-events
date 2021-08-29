a8cd524aeaa1481434c6d5f2654b6e995f101ce6
hudaokeji <github@hudaokeji.com>
2021-08-29 11:21:30 +0900

    rename module file name

---
+++ dist/skylark-langx-events/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-langx-events - The skylark JavaScript language extension library.
+ * @author Hudaokeji Co.,Ltd
+ * @version v0.9.0
+ * @link www.skylarkjs.org
+ * @license MIT
+ */
+define(["./events","./event","./listener","./emitter","./create-event"],function(e){return e});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ dist/uncompressed/skylark-langx-events/main.js
@@ -0,0 +1,9 @@
+define([
+	"./events",
+	"./event",
+	"./listener",
+	"./emitter",
+	"./create-event"
+],function(events){
+	return events;
+});
